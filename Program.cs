44b6104e342e294011987f6f0aa2392f5f1cf30f
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-02 12:53:30 -0700

    hashCode

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace hashCode
+{
+    internal class Person
+    {
+
+        public string firstName;
+        public string lastName;
+
+        public Person(string f_Name, string l_Name)
+        {
+            this.firstName = f_Name;
+            this.lastName = l_Name;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("first Name : "+ firstName);
+
+            Console.WriteLine("last Name : " + lastName);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ahmed", "Al Adhary");
+
+            Console.WriteLine("Person details:");
+            person.Show();
+
+            // Using GetHashCode() method
+            Console.WriteLine("The hash code of object is: {0}", person.GetHashCode());
+        }
+    }
+}
